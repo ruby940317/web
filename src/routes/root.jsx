567e91a8d46c7bb1d6b56d808b16144b839a6bf5
JUAN SHANG-ER <rcc0901367355@gmail.com>
2024-12-17 11:41:56 +0800

    update

---
+++ src/routes/root.jsx
@@ -0,0 +1,11 @@
+import {Icon} from '@iconify/react';
+import {toast} from "react-toastify";
+
+export default function Root() {
+    return (
+        <>
+        <Icon icon="fa6-regular:face-smile" />
+            <h1 className="italic ">t132132est</h1>
+        </>
+    );
+}
